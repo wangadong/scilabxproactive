0279ae919b1f4179e3dd81b259724a3e8ace6d7d
remiliuzhibin@gmail.com <remiliuzhibin@gmail.com@35df50d9-77a4-419f-d72f-b26d05d1b1e0>
2011-09-04 09:36:57 +0000

    可执行的版本

--- ScilabProxProActive/src/com/scilab/scheduler/ResolveScilabCode.java
+++ ScilabProxProActive/src/com/scilab/scheduler/ResolveScilabCode.java
@@ -1,6 +1,8 @@
 package com.scilab.scheduler;
 
+import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.ow2.proactive.scheduler.common.task.TaskResult;
@@ -12,15 +14,48 @@ public class ResolveScilabCode extends JavaExecutable{
     
 	@Override
 	public void init(Map<String,Serializable> args){
-    	scilabCode=(String) args.get("scilabCode");
+    	try {
+			super.init(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 	
 	@Override
 	public Serializable execute(TaskResult... results) throws Throwable {
+        try {
+            System.err.println("开始执行sci代码");
+            System.out.println("Parameters are : ");
+
+            for (TaskResult tRes : results) {
+                if (tRes.hadException()) {
+                    System.out.println("\t " + tRes.getTaskId() + " : " + tRes.getException().getMessage());
+                } else {
+                    System.out.println("\t " + tRes.getTaskId() + ": " + tRes.value());
+                }
+            }
+            String savePath="E:\\res.out";
+			Process process = Runtime.getRuntime().exec(
+					"cmd /c java -jar C:/ScilabDistribution/executeV2.jar"
+							+ " \"" + scilabCode + "\" " + "\"" + savePath + "\" "
+							+ "\"" + scilabCode + "\">\"" + savePath + "\"");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Task terminated");
-
-        return (scilabCode);
+        ArrayList<File> files=new ArrayList<File>();
+        files.add(new File("E:/res.out"));
+        files.add(new File("E:/figure.png"));
+        return (files);
 	}
-
+	
+	public String getScilabCode(){
+		return scilabCode;
+	}
+	public void setScilabCode(String scilabCode){
+		this.scilabCode=scilabCode;
+	}
 }
